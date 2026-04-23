2ff79b97fa1696e02c633f85f12f2661c64c8525
vk57434 <vk4492020@gmail.com>
2026-04-23 15:07:38 +0530

    remove withCredentials

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://conferencemanagementsys-backend.onrender.com/api",
-  withCredentials: true
+  
 });
 
 export default API;
